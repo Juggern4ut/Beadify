a192864055558ad42624ac3a9a038240452ca56a
Lukas Meier <lukas.meier94@gmail.com>
2020-01-24 13:49:30 +0100

    Improved design and first run

--- js/main.js
+++ js/main.js
@@ -28,11 +28,6 @@ const redrawSelectedColors = () => {
 };
 
 window.onload = () => {
-  for (let i = 0; i < 10; i++) {
-    colorArray.push(new Color().setHslValue(0, 0, i * 10));
-  }
-
-  redrawSelectedColors();
 
   let colorPicker = new ColorPicker();
 
@@ -59,4 +54,42 @@ window.onload = () => {
   document.getElementById("color-button").addEventListener("click", e => {
     lightbox.open();
   });
+
+  const infoLightbox = new Lightbox({
+    additionalClasses: ["align-right"],
+    openAnimation: "buildup",
+    closeAnimation: "collapse",
+    draggable: true
+  })
+    .setTitle("Color compare methods")
+    .setContent(
+      `
+      <h2>What are color compare methods?</h2>
+      <p>
+        Colors can be defined by several values. One is RGB which defines
+        the value for red, green and blue with a value between 0 and 255 
+        where 0 is no amount of that color and 255 is all the value of 
+        that color. rgb(255, 255, 0) for example would be a mix of all 
+        red and all green with no blue, so it would result in a bright 
+        yellow.
+      </p>
+      <p>Another way is hsl which stands for hue, saturation and lightness.</p>
+      <p>
+        Since there are several ways to look at color, there are also
+        more than one distinct way to compare colors to oneanother.
+        And with this tool you can experiment with three of them
+      </p>
+      <ul>
+        <li>RGB - Will compare the colors by the squared average of all color values added. This one will usually provide the best results for this task</li>
+        <li>Hue - Will compare by the hue of the color. Saturation and lightness however will not be taken into consideration here.</li>
+        <li>HSL - The same as Hue but will also take saturation and lightness into effect</li>
+      </ul>
+      <p>Feel free to experiment with different color combinations and compare methods</p>
+      `
+    );
+
+  const handler = document.getElementById("info_handler");
+  handler.onclick = () => {
+    infoLightbox.open();
+  };
 };
--- js/partials/_loadSampleImage.js
+++ js/partials/_loadSampleImage.js
@@ -2,7 +2,7 @@ let drawing = new Image();
 let canvas_width = 0;
 let canvas_height = 0;
 
-drawing.src = "../res/sample.jpg";
+drawing.src = "/res/sample.jpg";
 
 drawing.addEventListener("load", () => {
   canvas_height = drawing.naturalHeight;
@@ -15,5 +15,12 @@ drawing.addEventListener("load", () => {
   canvas_result.setAttribute("width", canvas_width);
 
   context_source.drawImage(drawing, 0, 0);
+
+  for (let i = 0; i < 10; i++) {
+    colorArray.push(new Color().setHslValue(0, 0, i * 10));
+  }
+
+  redrawSelectedColors();
+
   generateDrawing(20);
 });
